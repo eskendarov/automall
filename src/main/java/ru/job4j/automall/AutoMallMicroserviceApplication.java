70b63fc1083f157c9aef499b6c8067cf4256fa98
Enver Eskendarov <envereskendarov@gmail.com>
2024-01-05 12:05:37 -0800

    added to pom.xml maven dependencies

---
+++ src/main/java/ru/job4j/automall/AutoMallMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package ru.job4j.automall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMallMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoMallMicroserviceApplication.class, args);
+    }
+}
--- src/main/java/ru/job4j/automall/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.automall;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-    }
-}
